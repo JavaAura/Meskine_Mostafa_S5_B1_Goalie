e15c1c7f2ac3f7bd032b576d9a2e5dd78b529abc
MesVortex <meskinemostafa4@gmail.com>
2024-12-29 23:06:39 +0100

    Fix some responsive design issues

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgClass, NgIf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgOptimizedImage,
+    NgIf,
+    NgClass
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMobileMenuOpen: boolean = false;
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
